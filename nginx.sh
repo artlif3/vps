35904285d9707568be9e85192c79605ea520375d
KinGmapua <29995733+Vpaproject@users.noreply.github.com>
2021-12-21 22:37:50 +0800

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -38,7 +38,7 @@ http {
 }
 END3
 mkdir -p /home/vps/public_html
-wget -O /home/vps/public_html/index.html "http://script.hostingtermurah.net/repo/index.html"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/GakodArmy/teli/main/index.html"
 echo "<?phpinfo(); ?>" > /home/vps/public_html/info.php
 args='$args'
 uri='$uri'
